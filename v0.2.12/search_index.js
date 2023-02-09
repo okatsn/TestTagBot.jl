335f9c801c615fb720d6f34d4b17ef7a1a028471
Documenter.jl <documenter@juliadocs.github.io>
2023-02-09 02:39:20 +0000

    build based on 7264927

---
+++ v0.2.12/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TestTagBot","category":"page"},{"location":"#TestTagBot","page":"Home","title":"TestTagBot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TestTagBot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TestTagBot]","category":"page"}]
+}
